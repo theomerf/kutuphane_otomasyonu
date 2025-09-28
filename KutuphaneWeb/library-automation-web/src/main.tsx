be30fcce0b92d289986e2f32aed9ea3982f1528c
Omer Faruk Yalcin <omerfarukyalcin08@gmail.com>
2025-09-28 18:10:54 +0300

    QueryUrl2

--- KutuphaneWeb/library-automation-web/src/main.tsx
+++ KutuphaneWeb/library-automation-web/src/main.tsx
@@ -7,12 +7,12 @@ import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')!).render(
-
+  <StrictMode>
     <>
-    <ToastContainer position="bottom-right" theme='colored' autoClose={3000} />
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <ToastContainer position="bottom-right" theme='colored' autoClose={3000} />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </>
-
+  </StrictMode>
 )
